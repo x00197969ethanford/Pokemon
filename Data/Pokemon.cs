90bf8614d5612743c17c9badb94f494ad76a581b
x00197969ethanford <x00197969@mytudublin.ie>
2025-12-07 17:31:51 +0000

    Add JSON

--- Data/Pokemon.cs
+++ Data/Pokemon.cs
@@ -1,10 +1,19 @@
+using System.Text.Json.Serialization;
+
 namespace pokemon_project.Data
 {
     public class Pokemon
     {
+        [JsonPropertyName("id")]
         public int Id { get; set; }
-        public string Name { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; } = string.Empty;
+
+        [JsonPropertyName("height")]
         public int Height { get; set; }
+
+        [JsonPropertyName("weight")]
         public int Weight { get; set; }
 
     }
