862ec84bd42bb93b32b6183e4a773cc70a565d2a
x00197969ethanford <x00197969@mytudublin.ie>
2025-12-07 17:26:03 +0000

    start Pokemon class

---
+++ Data/Pokemon.cs
@@ -0,0 +1,11 @@
+namespace pokemon_project.Data
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Height { get; set; }
+        public int Weight { get; set; }
+
+    }
+}
--- obj/Debug/net9.0/pokemon_project.AssemblyInfo.cs
+++ obj/Debug/net9.0/pokemon_project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pokemon_project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e596a5a15b89f456ab17cc6a06a01085bddb0b6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("pokemon_project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pokemon_project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
