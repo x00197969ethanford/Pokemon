17909d34ae8730f91b7ca9f005298ddf88e9b22b
x00197969ethanford <x00197969@mytudublin.ie>
2025-12-07 22:11:23 +0000

    show stat name

--- obj/Debug/net9.0/pokemon_project.AssemblyInfo.cs
+++ obj/Debug/net9.0/pokemon_project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pokemon_project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d154df03b3bbd87fed90acd4cdfbd9c6f206b645")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+069744d7d81419353279815d836807f31318ada2")]
 [assembly: System.Reflection.AssemblyProductAttribute("pokemon_project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pokemon_project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
