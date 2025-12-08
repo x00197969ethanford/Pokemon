60480e32b31859822dc7f548487843cb0b240bbf
x00197969ethanford <x00197969@mytudublin.ie>
2025-12-08 15:15:17 +0000

    feet & inches working

--- obj/Debug/net9.0/pokemon_project.AssemblyInfo.cs
+++ obj/Debug/net9.0/pokemon_project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pokemon_project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d89bfd5a9c9cb2e9382aa8accc4780a3d554b1e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b473e9995316cb50f82847a984e5b211b79283e")]
 [assembly: System.Reflection.AssemblyProductAttribute("pokemon_project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pokemon_project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
