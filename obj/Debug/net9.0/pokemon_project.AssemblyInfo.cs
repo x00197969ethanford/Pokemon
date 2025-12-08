6bf5543bc24953da12fbbce7a4b7a2ddf6051861
x00197969ethanford <x00197969@mytudublin.ie>
2025-12-08 12:36:42 +0000

    find match tested and working

--- obj/Debug/net9.0/pokemon_project.AssemblyInfo.cs
+++ obj/Debug/net9.0/pokemon_project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pokemon_project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a34514ad7aed46523fd9f41c8044dfd11a00b43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b5dd10e5ed9c7530fef6b305ca9c92d203fd25d")]
 [assembly: System.Reflection.AssemblyProductAttribute("pokemon_project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pokemon_project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
