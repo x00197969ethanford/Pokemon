433810a773f4da49598a3d07c9d7dbb7b1cc8479
x00197969ethanford <x00197969@mytudublin.ie>
2025-12-08 20:10:28 +0000

    Set up pokemon test

---
+++ Pokemon.Tests/BasicTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Pokemon.Tests
+{
+    public class BasicTests
+    {
+        [Fact]
+        public void SimpleMathTest()
+        {
+            Assert.Equal(4, 2 + 2);
+        }
+    }
+}
