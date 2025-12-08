2776d0fd8e615e1d27c1c04f822bee3c6bdd41a7
x00197969ethanford <x00197969@mytudublin.ie>
2025-12-08 17:47:01 +0000

    Add error message & ranges

--- Layout/models/UserStats.cs
+++ Layout/models/UserStats.cs
@@ -4,8 +4,16 @@ namespace pokemon_project.Models
 {
     public class UserStats
     {
+        [Required(ErrorMessage = "Height (feet) is required.")]
+        [Range(0, 8, ErrorMessage = "Feet must be between 0 and 8.")]
         public int HeightFeet { get; set; }
+
+        [Required(ErrorMessage = "Height (inches) is required.")]
+        [Range(0, 11, ErrorMessage = "Inches must be between 0 and 11.")]
         public int HeightInches { get; set; }
+
+        [Required(ErrorMessage = "Weight is required.")]
+        [Range(1, 1000, ErrorMessage = "Weight must be between 1 and 1000 kg.")]
         public double WeightKg { get; set; }
     }
 }
