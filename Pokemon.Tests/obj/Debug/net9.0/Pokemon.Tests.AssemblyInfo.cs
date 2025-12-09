8b151a8e328cfb988b7b428d66f21992806fc860
x00197969ethanford <x00197969@mytudublin.ie>
2025-12-09 16:54:40 +0000

    start prefill option

--- Pokemon.Tests/obj/Debug/net9.0/Pokemon.Tests.AssemblyInfo.cs
+++ Pokemon.Tests/obj/Debug/net9.0/Pokemon.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokemon.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70ff4ffe85e322875bec4b6d1154748efd7d5c91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de42ff741712260c2d0864e7860395ff57cb13f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokemon.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokemon.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
