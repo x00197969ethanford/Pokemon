8a9418093d90e507f5d17f532158a12770997d4a
x00197969ethanford <x00197969@mytudublin.ie>
2025-12-09 10:54:53 +0000

    find closest match test works

--- Pokemon.Tests/BasicTests.cs
+++ Pokemon.Tests/BasicTests.cs
@@ -30,6 +30,16 @@ namespace PokemonProject.Tests
             new Pokemon { Name = "C", Height = 30, Weight = 30 },
             new Pokemon { Name = "D", Height = 40, Weight = 40 },
         };
+
+            var service = new PokemonService(new HttpClient());
+
+            var result = service.FindClosestMatches(pokemons, 200, 20);
+
+            var top3 = result.top3;
+
+            Assert.Equal(3, top3.Count); // needs 3 rresults
+            Assert.True(top3[0].score <= top3[1].score);
+            Assert.True(top3[1].score <= top3[2].score);
         }
     }
 }
--- Pokemon.Tests/obj/Debug/net9.0/Pokemon.Tests.AssemblyInfo.cs
+++ Pokemon.Tests/obj/Debug/net9.0/Pokemon.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokemon.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a79edea890210c8f85e9076c951b11e63c46cb93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdca9bfb1158ee2afdd69500388b89180cdde78d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokemon.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokemon.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
